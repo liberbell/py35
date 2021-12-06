33e7467120d8f91166d2a2827d71b290e40b1b43
Liberbell <liberbell@gmail.com>
2021-12-06 15:35:01 +0900

    hello3

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@ print(35000-15000)
 
 gross_salary = 1000000
 print(gross_salary)
+
+income_tax = 50000
+retirement_contribution = 1800
+professional_tax = 300
+
+net_salary = gross_salary - (income_tax + retirement_contribution + professional_tax)
+print(net_salary)
