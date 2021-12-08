87c12a5c592dcf7139c47726a626e0e259dc56e0
Liberbell <liberbell@gmail.com>
2021-12-08 10:00:27 +0900

    typeconvert4

--- typeconvert.py
+++ typeconvert.py
@@ -2,3 +2,8 @@ intevew_scores = {"Bob": 45, "Elton": 20, "Eric": 44}
 print(intevew_scores)
 print(type(intevew_scores))
 print(intevew_scores["Elton"])
+
+employee_map = {1001: "Bob", 1002: "Elton", 1003: "Eric"}
+print(employee_map)
+print(type(employee_map))
+print(employee_map[1002])
