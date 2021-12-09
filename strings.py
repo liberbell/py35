13c2deea5deed307c3ed62d0ad4dad15cb95062c
Liberbell <liberbell@gmail.com>
2021-12-09 09:44:58 +0900

    strings4

--- strings.py
+++ strings.py
@@ -1,5 +1,8 @@
 string1 = 'Python allow you to define strings using single quotes'
 print(string1)
 
-string2 = "Python allow you to define strings using single quotes"
+string2 = "Python allow you to define strings using double quotes"
 print(string2)
+
+string3 = """Python allow you to define strings using triple quotes"""
+print(string3)
