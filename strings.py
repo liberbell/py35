d09b017cb4f6c727dd8dcf4537d8741644fa1591
Liberbell <liberbell@gmail.com>
2021-12-10 14:37:24 +0900

    strings3

--- strings.py
+++ strings.py
@@ -49,4 +49,4 @@ time_spent = 2
 print("I love watching {} on TV - yesterday for {} hours.".format(favorite_sport, time_spent))
 
 favorite_sport = "soccer"
-print("I love watching %s on TV - yesterday for %d hours." % favorite_sport, time_spent)
+print("I love watching %s on TV - yesterday for %d hours." % favorite_sport, time_spent, "hello")
