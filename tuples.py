14b05ea36a102a121cf459d0681e798d22833112
Liberbell <liberbell@gmail.com>
2021-12-12 12:08:25 +0900

    tuples7

--- tuples.py
+++ tuples.py
@@ -10,3 +10,6 @@ print(integer_tuples[1])
 float_tuple = (2.4, 30.1, 100.2, 331.9)
 print(float_tuple, type(float_tuple))
 print(len(float_tuple))
+
+game_list = ["Badminton", "Golf", "Basketball", "Tennis"]
+print(game_list, type(game_list))
