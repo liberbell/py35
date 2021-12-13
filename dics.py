92610e4d0b9c60fff61f0734647b95330d4e0c8b
Liberbell <liberbell@gmail.com>
2021-12-14 08:28:14 +0900

    dics3

--- dics.py
+++ dics.py
@@ -1,2 +1,9 @@
 my_dict = {}
 print(type(my_dict), my_dict)
+
+my_employee = {"Bob": 4000,
+               "Eric": 5000,
+               "Alex": 2500,
+               "Jhon": 5500}
+
+print(my_employee)
