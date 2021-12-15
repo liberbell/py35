2eedaa2cc41aab67ecd0ceec370257cfb566c7cf
Liberbell <liberbell@gmail.com>
2021-12-15 09:49:09 +0900

    copies7

--- copies.py
+++ copies.py
@@ -14,3 +14,6 @@ print(first_copy)
 first_list[0] = "pinapple"
 print("Original:", first_list)
 print("Copy:   :", first_copy)
+
+first_person = ["Bob", ["1394 West Zenith St", "Massachusetts"]]
+print(first_person)
