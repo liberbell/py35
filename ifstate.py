93201c0ec703b93d2180830b8699d3fbc5d836df
Liberbell <liberbell@gmail.com>
2021-12-17 08:50:56 +0900

    ifstate3

--- ifstate.py
+++ ifstate.py
@@ -1,4 +1,11 @@
-if True:
-    print("This condition is always true, which means this code will always run!")
+# if True:
+#     print("This condition is always true, which means this code will always run!")
 if False:
     print("This condition is always false, which means this code will never run!")
+    
+if 1000 > 100:
+    print("1000 is greater than 100")
+    
+gross_salary = 500000
+if gross_salary >= 50000:
+    tax_rate = 30
