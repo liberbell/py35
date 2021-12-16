84c0b93d15e99f4362d5da46ff9a2e26d8ab94b8
Liberbell <liberbell@gmail.com>
2021-12-17 08:48:48 +0900

    ifstate2

--- ifstate.py
+++ ifstate.py
@@ -1,2 +1,4 @@
 if True:
     print("This condition is always true, which means this code will always run!")
+if False:
+    print("This condition is always false, which means this code will never run!")
