633dcd261f65c5a04b9009b70618426d40a432cf
Liberbell <liberbell@gmail.com>
2021-12-17 08:53:41 +0900

    ifstate6

--- ifstate.py
+++ ifstate.py
@@ -7,13 +7,13 @@ if 1000 > 100:
     print("1000 is greater than 100")
     
 gross_salary = 500000
-if gross_salary >= 50000:
+if gross_salary >= 500000:
     tax_rate = 30
 print(tax_rate)
 del tax_rate
 # print(tax_rate)
 
 gross_salary = 400000
-if gross_salary >= 50000:
+if gross_salary >= 500000:
     tax_rate = 30
 print(tax_rate)
