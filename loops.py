70757e581f40b63319e5d27192ad8d375217fe77
Liberbell <liberbell@gmail.com>
2021-12-18 10:45:37 +0900

    loops4

--- loops.py
+++ loops.py
@@ -3,3 +3,7 @@ for letters in ["W", "e", "l", "c", "o", "m", "e"]:
     
 for letters in "Welcome":
     print(letters)
+    
+var_string = "Welcome to New York"
+for char in var_string:
+    print(char)
