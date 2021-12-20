87fbdff6fcc4c400325725299d4149741e754267
Liberbell <liberbell@gmail.com>
2021-12-21 08:44:46 +0900

    netsloop2

--- nestloop.py
+++ nestloop.py
@@ -42,3 +42,9 @@ print(my_list)
 
 for my_num in range(1, 7):
     print(my_num, "cube is", my_num ** 3)
+
+my_list = []
+for i in range(5, 20):
+    if (i % 2 or i % 4 == 0):
+        my_list.append(i)
+print(my_list)
