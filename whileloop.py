82e98635ad381aa30083d81abf6c70dbaf23635d
Liberbell <liberbell@gmail.com>
2021-12-21 08:54:36 +0900

    whileloop5

--- whileloop.py
+++ whileloop.py
@@ -7,7 +7,16 @@ while num <= 10:
     print(num)
     num += 1
 
-num = 7
-while num >= 0:
-    print(num)
-    num -= 1
+# num = 7
+# while num >= 0:
+#     print(num)
+#     num -= 1
+
+sum_upto = 20
+i = 0
+total = 0
+while i <= sum_upto:
+    total += 1
+    i += 1
+
+print(f"Computed sum upto {sum_upto} and the result is {total}")
