d13828778b4136d5fdecd63b4a034a5492ed72af
Liberbell <liberbell@gmail.com>
2021-12-23 08:54:55 +0900

    breakpass2

--- breakpass.py
+++ breakpass.py
@@ -4,13 +4,13 @@
 #         break
     # print(letter)
 
-for num in [3, 11, 22, 35, 90]:
-    print(num)
+# for num in [3, 11, 22, 35, 90]:
+#     print(num)
 
-    if (num == 35):
-        print("The number %d has been found." % num)
-        print("Terminating the loop...")
-        break
+#     if (num == 35):
+#         print("The number %d has been found." % num)
+#         print("Terminating the loop...")
+#         break
 
 # places = ["New Zealand", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Paraguay"]
 # burglar_at = input("Enter the country where the burglar actually is: ")
@@ -23,22 +23,26 @@ for num in [3, 11, 22, 35, 90]:
 # else:
 #     print("The burglar got away...")
 
-letters = "James Bond"
-for letter in letters:
-    print(letter)
-    if letter in ["a", "i", "u", "e", "o"]:
-        continue
+# letters = "James Bond"
+# for letter in letters:
+#     print(letter)
+#     if letter in ["a", "i", "u", "e", "o"]:
+#         continue
 
-dogs = ["Labrador", "Golden Retriever", "Pug", "Greyhound", "Bulldog", "Cooker Spaniel"]
-for pet in dogs:
-    if pet == "Pug":
-        continue
-    if pet == "Bulldog":
-        break
-    print(pet)
+# dogs = ["Labrador", "Golden Retriever", "Pug", "Greyhound", "Bulldog", "Cooker Spaniel"]
+# for pet in dogs:
+#     if pet == "Pug":
+#         continue
+#     if pet == "Bulldog":
+#         break
+#     print(pet)
 
 
 for letter in "James Bond":
     if letter in ["a", "i", "u", "e", "o"]:
         pass
         print("We are in the pass block")
+    else:
+        print(letter)
+
+print("Finally done")
