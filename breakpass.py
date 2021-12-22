ef543382e2bcd63a012b29d03e315433ce63fa59
Liberbell <liberbell@gmail.com>
2021-12-22 09:01:59 +0900

    breakpass6

--- breakpass.py
+++ breakpass.py
@@ -6,3 +6,8 @@
 
 for num in [3, 11, 22, 35, 90]:
     print(num)
+
+    if (num == 35):
+        print("The number %d has been found." % num)
+        print("Terminating the loop...")
+        break
