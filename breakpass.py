699f6ff6e760f66b976db0adc05e251d21c52ea2
Liberbell <liberbell@gmail.com>
2021-12-22 09:06:51 +0900

    breakpass7

--- breakpass.py
+++ breakpass.py
@@ -11,3 +11,6 @@ for num in [3, 11, 22, 35, 90]:
         print("The number %d has been found." % num)
         print("Terminating the loop...")
         break
+
+place = ["New Zealand", "Norway", "Botswana", "Zimbabwe", "Uzbekistan", "Paraguay"]
+burglar_at = input("Enter the country where the burglar actually is: ")
