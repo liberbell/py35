e45dacd6a4b53724b361194a051b51d71f66dae1
Liberbell <liberbell@gmail.com>
2021-12-22 09:00:36 +0900

    breakpass5

--- breakpass.py
+++ breakpass.py
@@ -1,8 +1,8 @@
-for letter in "James Bond":
-    print(letter)
-    if letter == "o":
-        break
+# for letter in "James Bond":
+#     print(letter)
+#     if letter == "o":
+#         break
     # print(letter)
 
-    for num in [3, 11, 22, 35, 90]:
-        print(num)
+for num in [3, 11, 22, 35, 90]:
+    print(num)
