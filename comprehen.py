dc1eeadb18f71cce2b46b8832fee19beea48223e
Liberbell <liberbell@gmail.com>
2021-12-23 09:02:28 +0900

    comprehen2

--- comprehen.py
+++ comprehen.py
@@ -1,2 +1,10 @@
 numbers = [100, 200, 300, 400, 500, 600]
 print(numbers)
+
+numbers_copy = numbers
+print(numbers_copy)
+
+for number in numbers:
+    numbers_copy.append(number)
+
+print(numbers_copy)
