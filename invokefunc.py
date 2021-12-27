1455e1efe505e95d512a3fbff7d67d6542807a82
Liberbell <liberbell@gmail.com>
2021-12-28 08:43:54 +0900

    invokefunc1

---
+++ invokefunc.py
@@ -0,0 +1,6 @@
+print("A function is a set of operations that accept inputs.")
+print("Functions performe some specific computation and produce output")
+
+def my_function():
+    print("A function is a set of operations that accept inputs.")
+    print("Functions performe some specific computation and produce output")
