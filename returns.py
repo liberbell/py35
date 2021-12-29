cefb51d95f526b4b90938608f9edf99ffc1482e0
Liberbell <liberbell@gmail.com>
2021-12-29 10:33:34 +0900

    returns7

--- returns.py
+++ returns.py
@@ -11,3 +11,15 @@ print(type(result))
 result = square(10.22341)
 print(result)
 print(type(result))
+
+def calculate(num1, num2):
+    add = num1 + num2
+    sub = num1 - num2
+    mul = num1 * num2
+    div = num1 / num2
+
+    return add, sub, mul, div
+
+result = calculate(10, 2)
+print(result)
+print(type(result))
