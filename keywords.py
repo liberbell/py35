90697f007f92b56eddd7bc5687de958f8dd66a54
Liberbell <liberbell@gmail.com>
2021-12-30 14:55:07 +0900

    keywords6

--- keywords.py
+++ keywords.py
@@ -15,5 +15,8 @@ def create_student_record(name, age, major, gpa):
 student_1 = create_student_record("Eric", 23, "Computer Science", 3.8)
 print(student_1)
 
-student_2 = create_student_record("Alex", 4.6, 19, "Math")
+# student_2 = create_student_record("Alex", 4.6, 19, "Math")
+# print(student_2)
+
+student_2 = create_student_record(name = "Alex", age = 19, major = "Math", gpa = 4.6)
 print(student_2)
