23364f75954fae6e985391172b14e5acfdd36345
Liberbell <liberbell@gmail.com>
2021-12-30 14:50:29 +0900

    keywords3

--- keywords.py
+++ keywords.py
@@ -2,4 +2,12 @@ def order_food(name, food):
     print(f"My name is {name} and I have ordered {food}.")
 
 order_food("Eric", "peach")
-order_food("Alex")
+# order_food("Alex")
+
+def create_student_record(name, age, major, gpa):
+    return = {
+        "name": name,
+        "age": age,
+        "major": major,
+        "gpa": gpa
+    }
