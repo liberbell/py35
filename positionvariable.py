b854ecba7f5d2792e68b173edab0a83f6e2dbd30
Liberbell <liberbell@gmail.com>
2021-12-31 17:44:44 +0900

    position6

--- positionvariable.py
+++ positionvariable.py
@@ -8,3 +8,6 @@ display("Pyhon", "Ruby")
 languages_list = ["Python", "Java", "C++", "C#", "JavaScript"]
 display(languages_list)
 display(*languages_list)
+
+languages_list = ("Python", "Java", "C++", "C#", "JavaScript")
+display(*languages_list)
