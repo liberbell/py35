64c53964700769311f8e3ddab0d9f92d30226c96
Liberbell <liberbell@gmail.com>
2021-12-31 17:47:17 +0900

    position7

--- positionvariable.py
+++ positionvariable.py
@@ -11,3 +11,11 @@ display(*languages_list)
 
 languages_list = ("Python", "Java", "C++", "C#", "JavaScript")
 display(*languages_list)
+
+department_employee_mapping = {}
+def add_employe_to_department(department, *employee):
+    if department not in department_employee_mapping:
+        department_employee_mapping[department] = []
+
+    for employeein employees:
+        department_employee_mapping[department].append(employee)
