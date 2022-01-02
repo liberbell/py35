0aa907c7966e366083bd9b5fc6b9dcd010af0dcf
Liberbell <liberbell@gmail.com>
2022-01-02 13:56:43 +0900

    argpass4

--- argumentpass.py
+++ argumentpass.py
@@ -1,4 +1,17 @@
+# def print_student_details(name, age, gpa, enrolled):
+#     print("Name:", name)
+#     print("Age:", age)
+#     print("GPA:", gpa)
+#     print("Enrolled:", enrolled)
+
+# print_student_details("Bob", 24, 4.1, True)
+
 def print_student_details(name, age, gpa, enrolled):
+    name = name.upper()
+    age = age + 10
+    gpa = gpa - 0.3
+    enrolled = False
+    
     print("Name:", name)
     print("Age:", age)
     print("GPA:", gpa)
