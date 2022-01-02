ea8d1746e148cc08dfd7b0c46e978e57604126f5
Liberbell <liberbell@gmail.com>
2022-01-02 13:35:01 +0900

    printdetail3

--- print_details_of_files.py
+++ print_details_of_files.py
@@ -1,7 +1,7 @@
-order = "grocceries"
+order = "groceries"
 service = "Instacart"
 
-def print_details_of_files():
+def print_details_of_files(order, service):
     print("Order: ", order)
     print("Service: ", service)
 
