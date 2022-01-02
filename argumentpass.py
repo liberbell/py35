275ecf2f37f57f895b8f1b94f8f1d591826dd479
Liberbell <liberbell@gmail.com>
2022-01-02 14:01:31 +0900

    argpass5

--- argumentpass.py
+++ argumentpass.py
@@ -11,10 +11,18 @@ def print_student_details(name, age, gpa, enrolled):
     age = age + 10
     gpa = gpa - 0.3
     enrolled = False
-    
+
     print("Name:", name)
     print("Age:", age)
     print("GPA:", gpa)
     print("Enrolled:", enrolled)
 
 print_student_details("Bob", 24, 4.1, True)
+
+color_list = ["red", "black", "white", "blue", "yellow"]
+def reassign_list(color_list):
+    color_list = ["green", "gray"]
+    print("List inside function: ", color_list)
+
+reassign_list(color_list)
+print()
